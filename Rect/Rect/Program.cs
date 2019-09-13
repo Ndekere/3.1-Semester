5cef5b10ca2f347eecb65c0ee3d68bd74f6abc57
Ndekere254 <33548005+Ndekere@users.noreply.github.com>
2019-09-13 17:40:36 +0300

    C SHARP CODE
    - This code calculates the Area and perimeter of a rectangle using c sharp.
    - IDE used: Visual Studio 2017
    - Date code was pushed to GitHub: 13 - sep -2019

---
+++ Rect/Rect/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Rect {
+    class Program {
+        static void Main(String [] args) {
+            Console.Write("Enter the Length: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the width: ");
+            double width = Convert.ToDouble(Console.ReadLine());        
+            // print area of rectangle to console ...
+            Console.Write("The area is: " + length * width + Environment.NewLine);
+            // print perimeter of rectangle to console ...
+            Console.Write("The Perimeter is: " + 2 * (length + width) + "\r\n");
+        }
+ // NOTE:
+ /*
+  Use the Environment.NewLine command or "\r\n" to add spacing in the console output!
+         */
+    }
+
+}
