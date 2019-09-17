6676f25822279cfd5234503b0fbb74427f2b63a6
Ndekere254 <33548005+Ndekere@users.noreply.github.com>
2019-09-17 10:11:35 +0300

    C# code with A class and a Program
    Timestamp: 10:10 am East Africa Time || Sep-17-2019

---
+++ MathApp/MathApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathLibrary;
+
+namespace MathApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Maths maths = new Maths();
+            double l = 6, w = 5, r =7;
+            Console.WriteLine("Rect Area =" + maths.RectArea(l, w));
+            Console.WriteLine("Rect Per = " + maths.RectPer(l, w));
+            Console.WriteLine("Circle Area = " + maths.CircleArea(r));
+            Console.WriteLine("Circle Per = " + maths.CircleArea(r));
+            //Console.WriteLine(Math.Log());
+            //Console.WriteLine();
+        }
+    }
+}
---
+++ MathLibrary/MathLibrary/Maths.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    public class Maths
+    {
+        //constructor
+       // public Maths()
+       // {
+
+       // }
+        public double RectArea(double length, double width)
+        {
+            return length * width;
+        }
+            public double RectPer(double length, double width)
+        {
+                return 2 * (length + width);
+            }
+            public double CircleArea(double radius)
+        {
+                return 3.14 * radius * radius;
+            }
+            public double CirclePer(double radius)
+        {
+                return 2 * 3.14 * radius;
+            }
+        }
+    }
+
